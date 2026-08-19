777388f6f407a0d3b11be3256abaa0ebc9921385
Navratan8949 <navratan0443@gmail.com>
2026-08-19 11:33:15 +0530

    remove splash from code

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/sonner"
-import { SplashScreen } from "@/components/splash-screen/splash-screen"
 import { ReduxProvider } from "@/redux/Provider"
 import "./globals.css"
 
